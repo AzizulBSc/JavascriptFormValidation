1dbb825815c26402839d94f8e083154f8af6105b
AzizulHoque <azizulh8774@gmail.com>
2022-03-04 00:17:04 +0600

    css part completed

---
+++ js/index.js
@@ -0,0 +1,14 @@
+
+// selection part
+const username  = document.getElementById('username');
+const email = document.getElementById('email');
+const phone = document.getElementById('phone');
+const password = document.getElementById('password');
+const cpassword = document.getElementById('cpassword');
+const submit = document.getElementById('submit');
+
+
+submit.addEventListener('click', (e) => {
+    e.preventDefault();
+    
+});
